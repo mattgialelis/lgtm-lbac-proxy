0b349ed382d83869f7807fa7e36a92dd7f109b24
Matt Gialelis <gialelis.matthew.a@gmail.com>
2024-04-18 12:27:38 +0100

    Add License

--- pkg/satokengen/main.go
+++ pkg/satokengen/main.go
@@ -1,3 +1,8 @@
+// This code is licensed under the AGPL-3.0 License.
+// https://github.com/grafana/grafana/blob/main/LICENSE
+// Original code is from Grafana: https://github.com/grafana/grafana/tree/main/pkg/components/satokengen
+// All credit for the work is given back to Grafana
+
 package satokengen
 
 import (
